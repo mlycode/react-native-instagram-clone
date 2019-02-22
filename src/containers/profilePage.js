5e8759a6f90fb9d30094a8abc9683eeeeb8667f6
mlycode <m.ly@live.co.uk>
2019-02-22 16:11:15 +0000

    create basic layout for profile page

--- src/containers/profilePage.js
+++ src/containers/profilePage.js
@@ -1,21 +1,104 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
 
 
 export default class ProfilePage extends Component {
 
     render() {
         return (
+
             <View style={styles.container}>
-                <Text>profilePage</Text>
+                <ScrollView>
+                    <View style={styles.profileInfo}>
+                        <Text>Profile follows</Text>
+                    </View>
+
+                    <View style={styles.profileBio}>
+                        <Text>Bio text</Text>
+                    </View>
+
+                    <View style={styles.menuBar}>
+                        <Text>menu bar</Text>
+                    </View>
+
+                    <View style={styles.imageContainer}>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+                        <View style={styles.images}>
+
+                        </View>
+
+                    </View>
+                </ScrollView>
             </View>
+
+
         );
     }
 }
 
+let screenWidth = Dimensions.get('window').width;
+let screenHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "lightblue",
+        backgroundColor: "#FFFFFF",
         flex: 15
+    },
+
+    profileInfo: {
+        height: 100,
+        backgroundColor: 'pink'
+    },
+
+    profileBio: {
+        height: 100,
+        backgroundColor: 'lightblue'
+    },
+
+    menuBar: {
+        height: 50,
+        backgroundColor: 'lightgreen'
+    },
+
+    imageContainer: {
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+
+    images: {
+        width: screenWidth / 3 - 2,
+        height: screenWidth / 3,
+        backgroundColor: 'blue',
+        margin: 1
     }
 })
